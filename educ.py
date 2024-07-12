be4a608edc0278d66f5fe09c57e05f7af0eddba2
Gabriel Ribeiro <118030785+gabivirus@users.noreply.github.com>
2024-07-12 13:30:27 -0300

    primeiro arquivo de teste simples

---
+++ educ.py
@@ -0,0 +1,15 @@
+id_gn = {'soc_edu': 0, 'ori': 0}
+soc_edu = [
+    {
+        'id': id_gn['soc_edu'],
+        'nome': '',
+        'idade': int(),
+        
+
+    }
+    ]
+
+def id_ch(id_list):
+    id_lis.key[0] += 1
+
+    return print('Valor adicionado')
